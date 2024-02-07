fb56683e52896d968c680b9bdee562f12e57d6b0
mayu875 <shivangekarmayuri.com>
2024-02-07 23:45:39 +0530

    Initial Commit

---
+++ Student.java
@@ -0,0 +1,34 @@
+package com.example.student.model;
+
+public class Student {
+	
+	private String studId;
+	private String studName;
+	private String StudContact;
+	private String studAddress;
+	
+	public String getStudId() {
+		return studId;
+	}
+	public void setStudId(String studId) {
+		this.studId = studId;
+	}
+	public String getStudName() {
+		return studName;
+	}
+	public void setStudName(String studName) {
+		this.studName = studName;
+	}
+	public String getStudContact() {
+		return StudContact;
+	}
+	public void setStudContact(String StudContact) {
+		this.StudContact = StudContact;
+	}
+	public String getStudAddress() {
+		return studAddress;
+	}
+	public void setStudAddress(String studAddress) {
+		this.studAddress = studAddress;
+	}	
+}
